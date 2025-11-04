06eb4c7bd5352ac37cd45b88afbb91c7df6cc2cf
ezgna <fjpfbii4@gmail.com>
2025-11-04 05:30:51 -0800

    add nativewind and drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Drawer } from "expo-router/drawer";
+import "../global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Drawer>
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "Home",
+          title: "overview",
+        }}
+      />
+      <Drawer.Screen
+        name="example"
+        options={{
+          drawerLabel: "User",
+          title: "overview",
+        }}
+      />
+    </Drawer>
   );
 }
